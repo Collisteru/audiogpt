20c9cd07cd18606af884723d8b7b8dd1ca03234d
Anar Enkhzol <aukovien@gmail.com>
2023-03-04 22:52:20 -0700

    blob update test patch

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 import { Audio } from 'expo-av';
+import { AudioRecorder, AudioUtils } from 'react-native-audio-recorder-player';
 
 // BLOB COMPONENTS
 import { Svg, Circle, Defs, Mask, RadialGradient, Stop, Rect, Path} from 'react-native-svg';
@@ -17,72 +18,63 @@ import * as FileSystem from 'expo-file-system';
 export default function App() {
   const [recording, setRecording] = React.useState();
 
-  const particleWorld = [];
-  const velocityDecay = 0.01;
-  const particleLifetime = 1000.0;
-  let fftAvgWin = 0;
+  const [particleWorld, setParticleWorld] = useState([]);
+  const [fftAvgWin, setFftAvgWin] = useState(0);
 
   useEffect(() => {
-    const soundAllowed = async () => {
-      try {
-        const { status } = await Audio.requestPermissionsAsync();
-        if (status !== 'granted') {
-          throw new Error('Microphone permission not granted');
+    const audioRecorderPlayer = new AudioRecorder();
+    audioRecorderPlayer.setSubscriptionDuration(0.05); // set subscription duration to 50ms
+    audioRecorderPlayer.addRecordBackListener((e) => {
+      const audioData = JSON.parse(e.data);
+      const frequencyArray = audioData.data.map((d) => Math.abs(d)); // get absolute values of audio data
+      const fftAvg = frequencyArray.reduce((fftv, t) => fftv + t) / 255;
+      setFftAvgWin((prev) => (prev + fftAvg) / 2);
+    });
+    audioRecorderPlayer.startRecorder();
+
+    const circleCount = 4;
+    const initialParticleWorld = [];
+    for (let i = 1; i <= circleCount; i++) {
+      const initialRadius = circleCount / i;
+      initialParticleWorld.push({
+        particleId: i,
+        alive: true,
+        acceleration: i / circleCount,
+        velocity: i / circleCount,
+        radius: initialRadius,
+        birth: new Date().getTime() - i * 100,
+      });
+    }
+    setParticleWorld(initialParticleWorld);
+
+    const animationInterval = setInterval(() => {
+      const now = new Date().getTime();
+      const updatedParticleWorld = particleWorld.map((particle, index) => {
+        if (particle.alive) {
+          if (particle.radius > 50) {
+            particle.alive = false;
+          }
+          if (particle.birth < now - 1000) {
+            particle.alive = false;
+          }
+        } else {
+          particle.velocity = Math.min(fftAvgWin / 20, 0.9);
+          particle.radius = 0;
+          particle.alive = true;
+          particle.birth = new Date().getTime() - index * 100; // to maintain randomness
         }
-        const audioContent = new AudioContext();
-        const audioStream = await Audio.getAudioModeAsync();
-        const analyser = audioContent.createAnalyser();
-        audioStream.setAnalyser(analyser);
-        analyser.fftSize = 1024;
+        particle.velocity -= 0.01;
+        particle.radius += particle.velocity;
+        return particle;
+      });
+      setParticleWorld(updatedParticleWorld);
+    }, 50);
 
-        const frequencyArray = new Uint8Array(analyser.frequencyBinCount);
-
-        // we have 5 circles in our particle system
-        const circleCount = 4;
-        for (let i = 1; i <= circleCount; i++) {
-          const initialRadius = circleCount / i;
-          particleWorld.push({
-            particleId: i,
-            alive: true,
-            acceleration: i / circleCount,
-            velocity: i / circleCount,
-            radius: initialRadius,
-            birth: new Date().getTime() - i * 100,
-          });
-        }
-
-        const doDraw = () => {
-          requestAnimationFrame(doDraw);
-          analyser.getByteFrequencyData(frequencyArray);
-          const fftAvg =
-            frequencyArray.reduce((fftv, t) => fftv + t) / 255;
-          fftAvgWin = (fftAvgWin + fftAvg) / 2;
-          const radius = fftAvgWin / 20;
-          particleWorld.forEach((particle, index) => {
-            const now = new Date().getTime();
-            if (particle.alive) {
-              if (particle.radius > 50) {
-                particle.alive = false;
-              }
-              if (particle.birth < now - particleLifetime) {
-                particle.alive = false;
-              }
-            } else {
-              particle.velocity = Math.min(radius, 0.9);
-              particle.radius = 0;
-              particle.alive = true;
-              particle.birth = new Date().getTime() - index * 100; // to maintain randomness
-            }
-            particle.velocity -= velocityDecay;
-            particle.radius += particle.velocity;
-          });
-        };
-        doDraw();
-      } catch (error) {
-        console.log(error);
-      }
+    return () => {
+      clearInterval(animationInterval);
+      audioRecorderPlayer.stopRecorder();
+      audioRecorderPlayer.removeRecordBackListener();
     };
-    soundAllowed();
   }, []);
   
   async function startRecording() {
